36f59599c7764cdc10508a546df580da0123eba3
Florian Baaske <florianbaaske@Florians-MacBook-Pro-Arbeit.local>
2018-02-01 11:54:45 +0100

    finalize the script with comments and settings from a file

--- export_devices.ps1
+++ export_devices.ps1
@@ -1,13 +1,38 @@
+#Load Input Variables
+param (
+    [Parameter(Mandatory=$True)][string]$queryString,
+    [string]$filterTypeString=""
+)
+
 #Load the settings file and parse them
 #from http://tlingenf.spaces.live.com/blog/cns!B1B09F516B5BAEBF!213.entry
 #
 Get-Content ".\settings.cfg" | foreach-object -begin {$settings=@{}} -process { $k = [regex]::split($_,'='); if(($k[0].CompareTo("") -ne 0) -and ($k[0].StartsWith("[") -ne $True)) { $settings.Add($k[0], $k[1]) } }
 
+#save airwave settings to variables
 $username = $settings.user
 $password = $settings.password
 $airwavehost = $settings.airwave_host
-$network="1"
-$type_string="Aruba 2930F-24G-4SFP"
+
+#read export parameters
+$snmpCommunityString = $settings.snmp_community_string
+$type = $settings.type
+$authPassword = $settings.auth_password
+$snmpv3AuthProtocol = $settings.snmpv3_auth_protocol
+$privacyPassword = $settings.privacy_password
+$snmpv3PrivacyProtocol = $settings.snmpv3_privacy_protocol
+$snmpv3UserName = $settings.snmpv3_username
+$enablePassword = $settings.enable_password
+$snmpPort = $settings.snmp_port
+$telnetUsername = $settings.telnet_username
+$telnetPassword = $settings.telnet_password
+
+#check if a filter for Device Type is defined
+if($filterTypeString){
+    $type_string = $filterTypeString
+}else{
+    $type_string = $settings.filter_type_string
+}
 
 #set TLS Parameter
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
@@ -17,7 +42,6 @@ function Ignore-SelfSignedCerts
 {
     try
     {
-        Write-Host "Adding TrustAllCertsPolicy type." -ForegroundColor White
         Add-Type -TypeDefinition  @"
         using System.Net;
         using System.Security.Cryptography.X509Certificates;
@@ -31,7 +55,6 @@ function Ignore-SelfSignedCerts
             }
         }
 "@
-        Write-Host "TrustAllCertsPolicy type added." -ForegroundColor White
       }
     catch
     {
@@ -41,44 +64,55 @@ function Ignore-SelfSignedCerts
 }
 Ignore-SelfSignedCerts
 
+#Create the Login URL
 $loginUri = "https://" + $airwavehost + "/LOGIN"
+#Create the username password combination
 $body = "credential_0=" + $username + "&credential_1=" + $password + "&destination=/index.html"
 
+#Login to AirWave and save the session into the session variable
 $loginObj = Invoke-RestMethod -Uri $loginUri -Method Post -Body $body -ContentType 'application/x-www-form-urlencoded;charset=UTF-8' -SessionVariable session
 
-$apSearchUri = "https://" + $airwavehost + "/ap_search.xml?query=" + $network
+#Create the Search URL
+$apSearchUri = "https://" + $airwavehost + "/ap_search.xml?query=" + $queryString
 
+#Request the device list and save the answer to a variable
 $apsObj = Invoke-RestMethod -Uri $apSearchUri -WebSession $session
 
+#Convert the answer from AirWave to xml
 [xml]$apsXML = $apsObj
 
+#Create a new CSV object
 $csvlist=@()
 
+#Itterate through the list and search for the filter to create a new CSV row
 $apsXML.amp_ap_search.record |
 %{
     if($_.type_string.ascii_value -eq $type_string)
         {
             $newrow = New-Object PSObject -Property @{
                 "IP Address" = $_.icmp_address.ascii_value
-                "SNMP Community String" = "public"
+                "SNMP Community String" = $snmpCommunityString
                 "Name" = $_.name.ascii_value
-                "Type" = ""
-                "Auth Password" = ""
-                "SNMPv3 Auth Protocol" = ""
-                "Privacy Password" = ""
-                "SNMPv3 Privacy Protocol" = ""
-                "SNMPv3 Username" = ""
-                "Enable Password" = ""
-                "SNMP Port" = ""
-                "Telnet Username" = "admin"
-                "Telnet Password" = "admin"
+                "Type" = $type
+                "Auth Password" = $authPassword
+                "SNMPv3 Auth Protocol" = $snmpv3AuthProtocol
+                "Privacy Password" = $privacyPassword
+                "SNMPv3 Privacy Protocol" = $snmpv3PrivacyProtocol
+                "SNMPv3 Username" = $snmpv3UserName
+                "Enable Password" = $enablePassword
+                "SNMP Port" = $snmpPort
+                "Telnet Username" = $telnetUsername
+                "Telnet Password" = $telnetUsername
             }
             $csvlist += $newrow
         }
 }
 
+#Output the CSV to CLI
 $csvlist | ft -AutoSize
 
-$path = ".\" + $network + "switches.csv"
+#build the Path to the output file
+$path = ".\" + $queryString + "_devices.csv"
 
+#write the CSV list to the output file
 $csvlist | Select-Object "IP Address", "SNMP Community String", "Name", "Type", "Auth Password", "SNMPv3 Auth Protocol", "Privacy Password", "SNMPv3 Privacy Protocol", "SNMPv3 Username", "Telnet Username", "Telnet Password", "Enable Password", "SNMP Port" | export-csv -path $path -notypeinformation
