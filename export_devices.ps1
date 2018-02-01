cbbe5984cf4344cc956e7025c042a421f38ed74a
Florian Baaske <florianbaaske@Florians-MacBook-Pro-Arbeit.local>
2018-02-01 10:56:32 +0100

    load basic settings and save in variables

--- export_devices.ps1
+++ export_devices.ps1
@@ -1,6 +1,11 @@
-$username="admin"
-$password="admin"
-$airwavehost="airwave.flomain.local"
+#Load the settings file and parse them
+#from http://tlingenf.spaces.live.com/blog/cns!B1B09F516B5BAEBF!213.entry
+#
+Get-Content ".\settings.cfg" | foreach-object -begin {$settings=@{}} -process { $k = [regex]::split($_,'='); if(($k[0].CompareTo("") -ne 0) -and ($k[0].StartsWith("[") -ne $True)) { $settings.Add($k[0], $k[1]) } }
+
+$username = $settings.user
+$password = $settings.password
+$airwavehost = $settings.airwave_host
 $network="1"
 $type_string="Aruba 2930F-24G-4SFP"
 
